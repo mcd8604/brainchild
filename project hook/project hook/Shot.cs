c554f08408fbec007795ce3e1b5bc219548c0c34
eab6863 <eab6863@ee130bea-b940-0410-ae26-83e4380c6a04>
2007-12-20 23:20:20 +0000

    updated constructor

--- project hook/project hook/Shot.cs
+++ project hook/project hook/Shot.cs
@@ -32,13 +32,22 @@ namespace project_hook
 		#endregion // End of variables and Properties Region
 
 
-		public Shot(String p_Name, Vector2 p_Position, int p_Height, int p_Width, GameTexture p_Texture, float p_Alpha, bool p_Visible, float p_Degree, float p_Z, int p_Damage)
-            : base(p_Name, p_Position, p_Height, p_Width, p_Texture, p_Alpha, p_Visible, p_Degree, p_Z)
+		public Shot(String p_Name, Vector2 p_Position, int p_Height, int p_Width, GameTexture p_Texture, float p_Alpha, bool p_Visible, 
+							float p_Degree, float p_Z, Factions p_Faction, int p_Health, Path p_Path, int p_Speed, GameTexture p_DamageEffect,
+							float p_Radius, int p_Damage)
+            : base(p_Name, p_Position, p_Height, p_Width, p_Texture, p_Alpha, p_Visible, p_Degree, p_Z, p_Faction, p_Health, p_Path, p_Speed,
+					p_DamageEffect, p_Radius)
         {
 			Damage = p_Damage;
         }
 
 
-		
+		//public override void Update(float p_Elapsed)
+		//{
+		//    //
+		//    //
+		//    //
+		//    if(m_Faction==
+		//}
 	}
 }
