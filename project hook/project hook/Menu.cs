9a6b4c4b4c6160766a30007a5e25cb207c26418f
mcd8604 <mcd8604@users.noreply.github.com>
2007-12-21 00:04:58 +0000

    added Menu Manager, sprite creation in Menu

--- project hook/project hook/Menu.cs
+++ project hook/project hook/Menu.cs
@@ -1,10 +1,17 @@
 using System;
 using System.Collections;
+using System.Collections.Generic;
 using System.Text;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
 namespace project_hook
 {
+    /*
+     * Description: 
+     * 
+     * TODO:
+	 */
 	class Menu
 	{
 
@@ -34,32 +41,50 @@ namespace project_hook
 			}
 		}
 
-		private Sprite background;
-		private ArrayList menuSprites;
+		private String m_BackgroundName;
+		private Sprite m_BackgroundSprite;
 
-		public Menu(ArrayList menuItems)
+		private ArrayList m_MenuItemNames;
+		private ArrayList m_MenuItemSprites;
+
+		public Menu(ArrayList menuItemNames, String backgroundName)
 		{
 			m_selectedIndex = -1;
+			m_MenuItemNames = menuItemNames;
+			m_BackgroundName = backgroundName;			
+		}
 
-			menuSprites = new ArrayList();
-			for(int i = 0; i < menuItems.Capacity; i++)
+		public void Update()
+		{
+			GameTexture bgTexture = TextureLibrary.getGameTexture(m_BackgroundName, "");
+			m_BackgroundSprite = new Sprite(m_BackgroundName, new Vector2(200.0f, 200.0f), bgTexture.Height, bgTexture.Width, bgTexture, 100.0f, true, Depth.BackGround.Bottom, 0);
+
+			m_MenuItemSprites = new ArrayList();
+			for (int i = 0; i < m_MenuItemNames.Capacity; i++)
 			{
-				//Sprite s = new Sprite("blah");
-				//menuSprites.Add(s);
+				GameTexture curTexture = TextureLibrary.getGameTexture((String)m_MenuItemNames[i], "");
+				m_MenuItemSprites[i] = new Sprite((String)m_MenuItemNames[i], new Vector2(200.0f, 200.0f + (i * curTexture.Height)), curTexture.Height, curTexture.Width, curTexture, 100.0f, true, Depth.MidGround.Bottom, 0);
 			}
 		}
 
-		public void draw(SpriteBatch spriteBatch)
+		public void Draw(SpriteBatch m_SpriteBatch)
 		{
-			//draw background
-			background.Draw(spriteBatch);
-
-			//draw menu items
-			for (int i = 0; i < menuSprites.Capacity; i++)
+			m_BackgroundSprite.Draw(m_SpriteBatch);
+			for (int i = 0; i < m_MenuItemSprites.Capacity; i++)
 			{
-				//Sprite s = (Sprite)menuSprites[i];
-				//s.Draw(spriteBatch);
+				Sprite curSprite = (Sprite)m_MenuItemSprites[i];
+				curSprite.Draw(m_SpriteBatch);
 			}
+		}
+
+		public void testMouseClick(float mouseX, float mouseY)
+		{
+			//test if a menu item was clicked
+		}
+
+		public void testMouseMove(float mouseX, float mouseY)
+		{
+			//test if the mouse is over a menu item
 		}
 	
 	}
