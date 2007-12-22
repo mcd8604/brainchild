7a5a674f5ff1ca5dad3bf8a6e48ada64bb14659e
kao7002 <kao7002@ee130bea-b940-0410-ae26-83e4380c6a04>
2007-12-22 03:18:35 +0000

    Added another path

--- project hook/project hook/PathStrategy.cs
+++ project hook/project hook/PathStrategy.cs
@@ -1,13 +1,26 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.Xna.Framework;
 
 namespace project_hook
 {
 	public class PathStrategy
 	{
-		protected Dictionary<String, Object> m_Values;
-		public Dictionary<String, Object> Values
+      
+         public enum ValueKeys
+        {
+            Start,   //The starting point, Vector2
+            End,     //The Ending point 
+            Degree,  //The Degree of rotation, float 
+            Base,    //The that will recieve this information, Sprite 
+            Target,  //The target sprite, Sprite
+            Speed,   //The speed of the movement 
+            Duration //The duration of the movement.
+        }
+
+        protected Dictionary<ValueKeys, Object> m_Values;
+        public Dictionary<ValueKeys, Object> Values
 		{
 			get
 			{
@@ -20,12 +33,21 @@ namespace project_hook
 
 		}
 
-		public PathStrategy(Dictionary<String, Object> p_Values)
+        protected bool m_Done = false;
+        public bool isDone
+        {
+            get
+            {
+                return m_Done;
+            }
+        }
+
+        public PathStrategy(Dictionary<ValueKeys, Object> p_Values)
 		{
 			m_Values = p_Values;
 		}
 
-		public virtual void CalculateMovement(){
+		public virtual void CalculateMovement(GameTime p_gameTime){
 
 		}
 	}
