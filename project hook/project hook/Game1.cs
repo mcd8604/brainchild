5f9831be4a9bf40b4608e2d039fe1a24c65fc860
eab6863 <eab6863@ee130bea-b940-0410-ae26-83e4380c6a04>
2007-12-27 20:25:22 +0000

    i changed how a shot was made.

--- project hook/project hook/Game1.cs
+++ project hook/project hook/Game1.cs
@@ -182,55 +182,71 @@ namespace project_hook
                  shot.Y = back1.PlayerShip.Position.Y;
                  shot2Effect.Position = shot;	
                 */
-				//back1.Shoot();
-				
+                 Shot t_Shot = back1.Shoot(gameTime);
 
+                 if (t_Shot.Name.Equals("no_Shot"))
+                 {
+                     //used so when the weapon can't shoot
+                 }
+                 else
+                 {
+                     spritelist.Add(t_Shot);
+                 }
+                
             }
 
 			if (keyhandler.IsActionPressed(KeyHandler.Actions.PrimaryShoot))
 			{
-                             
-	                
-                Shot shot2 = new Shot("RedShot2", new Vector2(100, 100.0f), 75, 30, TextureLibrary.getGameTexture("RedShot", "1"), 100, true, 0, Depth.MidGround.Top
-                                        ,Collidable.Factions.Player,-1,null,2,null,5,10);
-                Vector2 shot = shot2.Position;
-                shot.X = back1.PlayerShip.Position.X;
-                shot.Y = back1.PlayerShip.Position.Y;
-                shot2.Position = shot;
+                //Shot shot2 = new Shot("RedShot2", new Vector2(100, 100.0f), 75, 30, TextureLibrary.getGameTexture("RedShot", "1"), 100, true, 0, Depth.MidGround.Top
+                //                        , Collidable.Factions.Player, -1, null, 2, null, 5, 10);
+                //Vector2 shot = shot2.Position;
+                //shot.X = back1.PlayerShip.Position.X;
+                //shot.Y = back1.PlayerShip.Position.Y;
+                //shot2.Position = shot;
 
-                shot2.setAnimation("RedShot", 10);
+                //shot2.setAnimation("RedShot", 10);
 
-                Dictionary<PathStrategy.ValueKeys, Object> dic = new Dictionary<PathStrategy.ValueKeys, object>();
-                dic.Add(PathStrategy.ValueKeys.Start, shot2.Center);
-                dic.Add(PathStrategy.ValueKeys.End, new Vector2(shot2.Center.X, -100));
-                dic.Add(PathStrategy.ValueKeys.Duration, -1.0f);
-                dic.Add(PathStrategy.ValueKeys.Base, shot2);
-                shot2.Path = new Path(Path.Paths.Shot, dic);
+                //Dictionary<PathStrategy.ValueKeys, Object> dic = new Dictionary<PathStrategy.ValueKeys, object>();
+                //dic.Add(PathStrategy.ValueKeys.Start, shot2.Center);
+                //dic.Add(PathStrategy.ValueKeys.End, new Vector2(shot2.Center.X, -100));
+                //dic.Add(PathStrategy.ValueKeys.Duration, -1.0f);
+                //dic.Add(PathStrategy.ValueKeys.Base, shot2);
+                //shot2.Path = new Path(Path.Paths.Shot, dic);
 
 
-                Shot shot1 = new Shot("RedShot", new Vector2(100.0f, 100.0f), 75, 30, TextureLibrary.getGameTexture("RedShot", "1"), 100, true, 0, Depth.MidGround.Top
-                                      ,Collidable.Factions.Player,-1,null,2,null,5,10);
+                //Shot shot1 = new Shot("RedShot", new Vector2(100.0f, 100.0f), 75, 30, TextureLibrary.getGameTexture("RedShot", "1"), 100, true, 0, Depth.MidGround.Top
+                //                      , Collidable.Factions.Player, -1, null, 2, null, 5, 10);
 
-                shot = shot1.Position;
-                shot.X = back1.PlayerShip.Position.X + 50;
-                shot.Y = back1.PlayerShip.Position.Y;
-                shot1.Position = shot;
+                //shot = shot1.Position;
+                //shot.X = back1.PlayerShip.Position.X + 50;
+                //shot.Y = back1.PlayerShip.Position.Y;
+                //shot1.Position = shot;
 
-                shot1.setAnimation("RedShot", 10);
+                //shot1.setAnimation("RedShot", 10);
 
-                dic = new Dictionary<PathStrategy.ValueKeys, object>();
-                dic.Add(PathStrategy.ValueKeys.Start, shot1.Center);
-                dic.Add(PathStrategy.ValueKeys.End, new Vector2(shot1.Center.X, -100));
-                dic.Add(PathStrategy.ValueKeys.Duration, -1.0f);
-                dic.Add(PathStrategy.ValueKeys.Base, shot1);
-                shot1.Path = new Path(Path.Paths.Shot, dic);
+                //dic = new Dictionary<PathStrategy.ValueKeys, object>();
+                //dic.Add(PathStrategy.ValueKeys.Start, shot1.Center);
+                //dic.Add(PathStrategy.ValueKeys.End, new Vector2(shot1.Center.X, -100));
+                //dic.Add(PathStrategy.ValueKeys.Duration, -1.0f);
+                //dic.Add(PathStrategy.ValueKeys.Base, shot1);
+                //shot1.Path = new Path(Path.Paths.Shot, dic);
 
-                shot1.Animation.StartAnimation();
-                shot2.Animation.StartAnimation();
+                //shot1.Animation.StartAnimation();
+                //shot2.Animation.StartAnimation();
+                
+                //spritelist.Add(back1.Shoot(gameTime));
+                //spritelist.Add(shot2);
+                Shot t_Shot = back1.Shoot(gameTime);
 
-                spritelist.Add(shot1);
-                spritelist.Add(shot2);
-				back1.Shoot(gameTime);
+                if (t_Shot.Name.Equals("no_Shot"))
+                {
+                    //used so when the weapon can't shoot
+                }
+                else
+                {
+                    spritelist.Add(t_Shot);
+                }
+                
 			}
 
             if (keyhandler.IsActionDown(KeyHandler.Actions.Right))
@@ -381,6 +397,9 @@ namespace project_hook
             drawtext.DrawString(m_spriteBatch, "Press Space!!!!", new Vector2(100, 100), Color.Yellow, Depth.ForeGround.Top);
 			drawtext.DrawString(m_spriteBatch, "Score: " + back1.Score.ScoreTotal, new Vector2(0,50),Color.White);
             drawtext.DrawString(m_spriteBatch, "FPS: " + fps.ToString(),Vector2.Zero,Color.White);
+            drawtext.DrawString(m_spriteBatch, "Last shot: " + back1.PlayerShip.Weapon.LastShot, new Vector2(0, 150), Color.White);
+            drawtext.DrawString(m_spriteBatch, "Delay: " + back1.PlayerShip.Weapon.Delay, new Vector2(0, 200), Color.White);
+            drawtext.DrawString(m_spriteBatch, "add " + (back1.PlayerShip.Weapon.LastShot + back1.PlayerShip.Weapon.Delay), new Vector2(0, 250), Color.White);
 			cloud.Draw(m_spriteBatch);
 
 			enemy.Draw(m_spriteBatch);
@@ -395,4 +414,6 @@ namespace project_hook
             base.Draw(gameTime);
         }
     }
+
+    
 }
