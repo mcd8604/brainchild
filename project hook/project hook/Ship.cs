ec4eb09e4961d8e5cdcfbfb8d70e9c6968db8d19
kao7002 <kao7002@ee130bea-b940-0410-ae26-83e4380c6a04>
2008-01-07 02:20:16 +0000

    Added some code to support multiple weapons on wone ship

--- project hook/project hook/Ship.cs
+++ project hook/project hook/Ship.cs
@@ -15,12 +15,14 @@ namespace project_hook
     public class Ship : Collidable 
     {
         //variable for the weapon that the ship currently has
-		Weapon m_Weapon; 
+		List<Weapon> m_Weapons; 
 
         public Ship(String p_Name, Vector2 p_Position, int p_Height, int p_Width, GameTexture p_Texture, float p_Alpha, bool p_Visible, float p_Degree, float p_zBuff, Factions p_Faction, int p_Health, Path p_Path, int p_Speed, GameTexture p_DamageEffect, float p_Radius)
             : base(p_Name, p_Position, p_Height, p_Width, p_Texture, p_Alpha, p_Visible, p_Degree, p_zBuff, p_Faction, p_Health, p_Path, p_Speed, p_DamageEffect, p_Radius)
         {
-			m_Weapon = new Weapon(this, 10, 800, 5, TextureLibrary.getGameTexture("RedShot", "1"));
+            m_Weapons = new List<Weapon>();
+			m_Weapons.Add(new Weapon(this, 10, 150, 15, TextureLibrary.getGameTexture("RedShot", "1")));
+
 			Sprite shield = new Sprite("Shield", Vector2.Zero, (int)(p_Width *1.30), (int)(p_Height * 1.30), TextureLibrary.getGameTexture("Shield",""), 100, true, 0, Depth.MidGround.Bottom);
             Dictionary<PathStrategy.ValueKeys, Object> dic = new Dictionary<PathStrategy.ValueKeys, object>();
             
@@ -40,15 +42,26 @@ namespace project_hook
 			
         }
 
-        public Weapon Weapon
+        public List<Shot> shoot(GameTime p_GameTime)
+        {
+            List<Shot> allShots = new List<Shot>(); ;
+            foreach (Weapon w in m_Weapons)
+            {
+                List<Shot> shots = w.CreatShot(p_GameTime);
+                allShots.AddRange(shots.GetRange(0, shots.Count));
+            }
+            return allShots;
+        }
+
+        public List<Weapon> Weapons
         {
             get
             {
-				return m_Weapon;
+				return m_Weapons;
             }
             set
             {
-				m_Weapon = value;
+				m_Weapons = value;
             }
         }
     }
