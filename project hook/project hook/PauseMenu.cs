725e121095b646785b3c0fc2cc1ecdc6dde2442b
mcd8604 <mcd8604@ee130bea-b940-0410-ae26-83e4380c6a04>
2008-01-16 00:57:22 +0000

    added support for TextSprites in Menu

--- project hook/project hook/PauseMenu.cs
+++ project hook/project hook/PauseMenu.cs
@@ -16,26 +16,37 @@ namespace project_hook
 			m_HighlightName = "menu_highlight";
 
 			m_MenuItemNames = new ArrayList();
-			m_MenuItemNames.Add("menu_restart");
-			m_MenuItemNames.Add("menu_exitgame");
-			m_MenuItemNames.Add("menu_quit");
+			//m_MenuItemNames.Add("menu_restart");
+			//m_MenuItemNames.Add("menu_exitgame");
+			//m_MenuItemNames.Add("menu_quit");
+
+			usingTextSprite = true;
+			m_MenuItemNames.Add("Resume");
+			m_MenuItemNames.Add("Restart Game");
+			m_MenuItemNames.Add("Exit to Main Menu");
+			m_MenuItemNames.Add("Quit to Windows");
 		}
 
 		public override void accept()
 		{
 			if (m_selectedIndex == 0)
 			{
-				World.CreateWorld = true;
 				Menus.setCurrentMenu(Menus.MenuScreens.None);
 			}
 
 			if (m_selectedIndex == 1)
 			{
+				World.CreateWorld = true;
+				Menus.setCurrentMenu(Menus.MenuScreens.None);
+			}
+
+			if (m_selectedIndex == 2)
+			{
 				World.DestroyWorld = true;
 				Menus.setCurrentMenu(Menus.MenuScreens.Main);
 			}
 
-			if (m_selectedIndex == 2)
+			if (m_selectedIndex == 3)
 			{
 				Menus.Exit = true; 
 			}
