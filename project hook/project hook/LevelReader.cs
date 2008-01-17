b966e1b5f5db3ea53ab4558ee5f7a253e2d742b1
eab6863 <eab6863@ee130bea-b940-0410-ae26-83e4380c6a04>
2008-01-17 02:48:32 +0000

    xml works but does not create a ship yet.

--- project hook/project hook/LevelReader.cs
+++ project hook/project hook/LevelReader.cs
@@ -116,23 +116,143 @@ namespace project_hook
 			Vector2 m_StartPos = new Vector2();
 			int m_Height;
 			int m_Width;
+
 			GameTexture m_Texture;
+			String gtName;
+			String gtTag = "";
+
 			float m_Alpha;
 			bool m_Visible;
 			float m_Degree;
 			float m_ZBuff;
-			//Factions m_Faction;
+			Collidable.Factions m_Faction;
 			int m_Health;
 			int m_Shield;
-			Path m_Path;
+
+			Dictionary<PathStrategy.ValueKeys, Object> dic = new Dictionary<PathStrategy.ValueKeys, object>();
+			//dic.Add(PathStrategy.ValueKeys.Base, enemy);
+			float pSpeed;//dic.Add(PathStrategy.ValueKeys.Speed, 100f);
+			Vector2 pEndPos = new Vector2();//dic.Add(PathStrategy.ValueKeys.End, new Vector2(700, 200));
+			float pDuration;//dic.Add(PathStrategy.ValueKeys.Duration, 5f);
+			String pType;
+
 			int m_Speed;
+
 			GameTexture m_DamageTexture;
+			String dtName;
+			String dtTag = "";
 			float m_Radius;
-			
+
 			//read in name
-			Console.WriteLine(p_Reader.ReadInnerXml());
-			m_Name = p_Reader.ReadInnerXml();
-		
+			p_Reader.ReadStartElement();
+			m_Name = p_Reader.ReadString();
+			p_Reader.ReadEndElement();
+			//Console.WriteLine(m_Name);
+
+			//read in start position x y
+			m_StartPos.X = int.Parse(p_Reader.GetAttribute(0));
+			m_StartPos.Y = int.Parse(p_Reader.GetAttribute(1));
+			//Console.WriteLine(m_StartPos.X);
+			//Console.WriteLine(m_StartPos.Y);
+
+			//read in height
+			p_Reader.ReadStartElement();
+			m_Height = int.Parse(p_Reader.ReadString());
+			p_Reader.ReadEndElement();
+			//Console.WriteLine(m_Height);
+
+			//read in width
+			p_Reader.ReadStartElement();
+			m_Width = int.Parse(p_Reader.ReadString());
+			p_Reader.ReadEndElement();
+			//Console.WriteLine(m_Width);
+			
+			//read in texture
+			gtName=p_Reader.GetAttribute(0);
+			if(p_Reader.AttributeCount>1)
+			{
+				gtTag=p_Reader.GetAttribute(1);
+			}
+			m_Texture = TextureLibrary.getGameTexture(gtName, gtTag);
+
+			//read in alpha
+			p_Reader.ReadStartElement();
+			m_Alpha = float.Parse(p_Reader.ReadString());
+			p_Reader.ReadEndElement();
+
+			//read in visible
+			p_Reader.ReadStartElement();
+			m_Visible = bool.Parse(p_Reader.ReadString());
+			p_Reader.ReadEndElement();
+
+			//read in degree
+			p_Reader.ReadStartElement();
+			m_Degree = float.Parse(p_Reader.ReadString());
+			p_Reader.ReadEndElement();
+
+			//read in zBuff
+			Console.WriteLine(p_Reader.Name);
+
+			//read in faction
+			p_Reader.ReadStartElement();
+			p_Reader.ReadString();
+			Console.WriteLine(p_Reader.Name);
+			p_Reader.ReadEndElement();
+
+			//read in health
+			p_Reader.ReadStartElement();
+			m_Health = int.Parse(p_Reader.ReadString());
+			p_Reader.ReadEndElement();
+
+			//read in shield
+			p_Reader.ReadStartElement();
+			m_Shield = int.Parse(p_Reader.ReadString());
+			p_Reader.ReadEndElement();
+
+			//read in speed
+			p_Reader.ReadStartElement();
+			m_Speed = int.Parse(p_Reader.ReadString());
+			p_Reader.ReadEndElement();
+			//Console.WriteLine(m_Speed);
+
+			//read in damage texture
+			dtName = p_Reader.GetAttribute(0);
+			if (p_Reader.AttributeCount>1)
+			{
+			    dtTag = p_Reader.GetAttribute(1);
+			}
+			m_DamageTexture = TextureLibrary.getGameTexture(dtName, dtTag);
+
+			//read in radius
+			p_Reader.ReadStartElement();
+			m_Radius = float.Parse(p_Reader.ReadString());
+			p_Reader.ReadEndElement();
+			//Console.WriteLine(m_Radius);
+
+			//create ship
+
+
+			//read in paths
+			do
+			{
+				//type
+				p_Reader.ReadStartElement("path");
+				pType = p_Reader.ReadString();
+				p_Reader.ReadEndElement();
+				Console.WriteLine(pType);
+				//x y
+				pEndPos.X = int.Parse(p_Reader.GetAttribute(0));
+				pEndPos.Y = int.Parse(p_Reader.GetAttribute(1));
+				//duration
+				p_Reader.ReadStartElement();
+				pDuration = float.Parse(p_Reader.ReadString());
+				p_Reader.ReadEndElement();
+				//speed
+				p_Reader.ReadStartElement();
+				pSpeed = int.Parse(p_Reader.ReadString());
+				p_Reader.ReadEndElement();
+				p_Reader.ReadEndElement();
+			} while (p_Reader.Name.Equals("path"));
 		}
 	}
 }
