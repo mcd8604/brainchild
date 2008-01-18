faf3697642446f391761cf5008c89e0eb883e571
adn1646 <adn1646@ee130bea-b940-0410-ae26-83e4380c6a04>
2008-01-18 00:41:22 +0000

    mouse click

--- project hook/project hook/RITLogo.cs
+++ project hook/project hook/RITLogo.cs
@@ -36,7 +36,7 @@ namespace project_hook
 
 			m_Time += p_Time.ElapsedGameTime.TotalSeconds;
 
-			if (InputHandler.IsActionPressed(Actions.Pause) || m_Time >= m_Delay)
+			if (InputHandler.IsActionPressed(Actions.Pause) || InputHandler.IsActionPressed(Actions.MenuAccept) || m_Time >= m_Delay)
 			{
 				Menus.setCurrentMenu(Menus.MenuScreens.Title);
 			}
