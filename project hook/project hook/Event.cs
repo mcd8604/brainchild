ae5bec37da810261966e52f2b116cd8735d69342
eab6863 <eab6863@ee130bea-b940-0410-ae26-83e4380c6a04>
2008-01-23 18:43:49 +0000

    can read a n xml file. can read events createShip, changeSpeed, and changeFile.

--- project hook/project hook/Event.cs
+++ project hook/project hook/Event.cs
@@ -5,36 +5,30 @@ using Microsoft.Xna.Framework;
 
 namespace project_hook
 {
-	public enum Type
-	{
-		create,
-		change,
-		next
-	}
-
 	public class Event
 	{
-		Type m_Type;
+		String m_Type;
 		Sprite m_Sprite;
 		int m_Speed;
 		String m_FileName;
 
-		public Event(Type p_Type, Dictionary<Events.ValueKeys, Object> p_Values)
+		public Event(Sprite p_Sprite)
 		{
-			m_Type = p_Type;
+			m_Type = "Sprite";
 
-			switch (m_Type)
-			{
-				case Type.change:
-					m_Speed = (int)p_Values[Events.ValueKeys.Speed];
-					break;
-				case Type.create:
-					m_Sprite = (Sprite)p_Values[Events.ValueKeys.Sprite];
-					break;
-				case Type.next:
-					m_FileName = (String)p_Values[Events.ValueKeys.FileName];
-					break;
-			}
+			m_Sprite = p_Sprite;
+		}
+		public Event(String p_FileName)
+		{
+			m_Type = "FileChange";
+
+			m_FileName = p_FileName;
+		}
+		public Event(int p_Speed)
+		{
+			m_Type = "ChangeSpeed";
+
+			m_Speed = p_Speed;
 		}
 	}
 }
