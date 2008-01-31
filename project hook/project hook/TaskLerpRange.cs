bab8932a9bc67b67ef6e1cb519e1be9549eb3851
adn1646 <adn1646@ee130bea-b940-0410-ae26-83e4380c6a04>
2008-01-31 03:24:18 +0000

    Tail Targeting Reticle, let me know what you think.

---
+++ project hook/project hook/TaskLerpRange.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace project_hook
+{
+	class TaskLerpRange : Task
+	{
+		private Sprite m_From = null;
+		public Sprite From
+		{
+			get
+			{
+				return m_From;
+			}
+			set
+			{
+				m_From = value;
+			}
+		}
+		private Sprite m_To = null;
+		public Sprite To
+		{
+			get
+			{
+				return m_To;
+			}
+			set
+			{
+				m_To = value;
+			}
+		}
+		private float m_Range = 0f;
+		public float Range
+		{
+			get
+			{
+				return m_Range;
+			}
+			set
+			{
+				m_Range = value;
+			}
+		}
+
+		public TaskLerpRange() { }
+		public TaskLerpRange(Sprite p_From, Sprite p_To, float p_MaxRangeFromFrom) {
+			From = p_From;
+			To = p_To;
+			Range = p_MaxRangeFromFrom;
+		}
+		protected override void Do(Sprite on, GameTime at)
+		{
+			float value = 1;
+			float distance = Vector2.Distance(From.Center, To.Center);
+			if ( distance > Range ) {
+				value = Range / distance;
+			}
+			on.Center = Vector2.Lerp(From.Center, To.Center, value);
+		}
+		public override Task copy()
+		{
+			throw new Exception("The method or operation is not implemented.");
+		}
+	}
+}
