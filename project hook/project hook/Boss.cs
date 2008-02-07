940309c19f6487e7d870b1436ec374521bfadcbd
mxd1365 <mxd1365@ee130bea-b940-0410-ae26-83e4380c6a04>
2008-02-07 22:30:49 +0000

    made boss more versital

--- project hook/project hook/Boss.cs
+++ project hook/project hook/Boss.cs
@@ -6,19 +6,62 @@ namespace project_hook
 {
 	class Boss : Ship
 	{
-		public Boss() { }
+		List<Event> m_EventList;
+		bool m_EventTrigger;
+		bool EventTrigger
+		{
+			get
+			{
+				return m_EventTrigger;
+			}
+			set
+			{
+				m_EventTrigger = value;
+			}
+		}
+
+		public Boss()
+		{
+			m_EventList = new List<Event>();
+			//temp
+			m_EventList.Add(new Event(80));
+		}
+
+		public Boss(List<Event> p_EventList)
+		{
+			m_EventList = p_EventList;
+		}
+			
 
 		public override void RegisterCollision(Collidable p_Other)
 		{
 #if DEBUG
 			Console.WriteLine("The Trigger has been hit by " + p_Other + "!");
 #endif
-			if (World.Position.Speed == 0 && p_Other.Faction == Factions.Player && !(p_Other is Tail))
+			base.RegisterCollision(p_Other);
+		}
+
+		public override void Update(Microsoft.Xna.Framework.GameTime p_Time)
+		{
+			base.Update(p_Time);
+			
+			//temp
+			if (World.Position.Speed == 0)
 			{
-				base.RegisterCollision(p_Other);
-				World.Position.setSpeed(80);
-				ToBeRemoved = true;
+				if (this.ToBeRemoved == true || this.Faction != Factions.Enemy)
+					EventTrigger = true;
+			}
+			//end temp
+
+			if (m_EventTrigger && m_EventList.Count > 0)
+			{
+				if (m_EventList[0].Type == "ChangeSpeed")
+					World.Position.setSpeed(m_EventList[0].Speed);
+
+				m_EventList.RemoveAt(0);
 			}
 		}
+
+		
 	}
 }
