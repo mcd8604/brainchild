16648e010666411099a44bd17a550cab08d297dc
mcd8604 <mcd8604@users.noreply.github.com>
2008-02-07 02:18:35 +0000

    ship parts woot

---
+++ project hook/project hook/ShipPart.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace project_hook
+{
+	class ShipPart : Ship
+	{
+		private bool m_TransfersDamage = false;
+		public bool TransfersDamage
+		{
+			get
+			{
+				return m_TransfersDamage;
+			}
+			set
+			{
+				m_TransfersDamage = value;
+			}
+		}
+
+		private Ship m_ParentShip = null;
+		public Ship ParentShip
+		{
+			get
+			{
+				return m_ParentShip;
+			}
+			set
+			{
+				m_ParentShip = value;
+			}
+		}
+
+		public ShipPart() { }
+
+		public override void RegisterCollision(Collidable p_Other)
+		{
+			if (m_TransfersDamage)
+			{
+				m_ParentShip.RegisterCollision(p_Other);
+			}
+			else
+			{
+				base.RegisterCollision(p_Other);
+			}			
+		}
+	}
+}
