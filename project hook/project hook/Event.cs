a40be476abf5609bc7d57d01a88cfca4db354bcc
adn1646 <adn1646@ee130bea-b940-0410-ae26-83e4380c6a04>
2008-02-07 22:15:50 +0000

    simplified level event handling

--- project hook/project hook/Event.cs
+++ project hook/project hook/Event.cs
@@ -7,13 +7,26 @@ namespace project_hook
 {
 	public class Event
 	{
-		protected String m_Type;
-		public String Type
+
+		public enum Types
+		{
+			CreateSprite,
+			ChangeFile,
+			ChangeSpeed,
+			LoadBMP
+		}
+
+		protected Types m_Type;
+		public Types Type
 		{
 			get
 			{
 				return m_Type;
 			}
+		}
+		public void setType(string type)
+		{
+			m_Type = (Types)Enum.Parse(typeof(Types), type, true);
 		}
 
 		protected Sprite m_Sprite;
@@ -45,20 +58,22 @@ namespace project_hook
 
 		public Event(Sprite p_Sprite)
 		{
-			m_Type = "Sprite";
-
+			m_Type = Types.CreateSprite;
 			m_Sprite = p_Sprite;
+		}
+		public Event(String p_FileName, Types p_Type)
+		{
+			m_Type = p_Type;
+			m_FileName = p_FileName;
 		}
 		public Event(String p_FileName, String p_Type)
 		{
-			m_Type = p_Type;
-
+			setType(p_Type);
 			m_FileName = p_FileName;
 		}
 		public Event(int p_Speed)
 		{
-			m_Type = "ChangeSpeed";
-
+			m_Type = Types.ChangeSpeed;
 			m_Speed = p_Speed;
 		}
 	}
