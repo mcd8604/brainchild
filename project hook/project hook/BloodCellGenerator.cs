d71f71e9f54ffbf9b37ed8099bfea88b02934326
mxd1365 <mxd1365@ee130bea-b940-0410-ae26-83e4380c6a04>
2008-02-13 18:44:59 +0000

    BloodCellGenerator takes range and max blood cells in list

--- project hook/project hook/BloodCellGenerator.cs
+++ project hook/project hook/BloodCellGenerator.cs
@@ -11,6 +11,44 @@ namespace project_hook
 	public class BloodCellGenerator
 	{
 		float m_BloodCellDelay = 3.0f;
+		public float BloodCellDelay
+		{
+			get
+			{
+				return m_BloodCellDelay;
+			}
+			set
+			{
+				m_BloodCellDelay = value;
+			}
+		}
+
+		int m_BloodCellMinSpawnRange = 100;
+		public int MinSpawnRange
+		{
+			get
+			{
+				return m_BloodCellMinSpawnRange;
+			}
+			set
+			{
+				m_BloodCellMinSpawnRange = value;
+			}
+		}
+
+		int m_BloodCellMaxSpawnRange = Game.graphics.GraphicsDevice.Viewport.Width - 100;
+		public int MaxSpawnRange
+		{
+			get
+			{
+				return m_BloodCellMaxSpawnRange;
+			}
+			set
+			{
+				m_BloodCellMaxSpawnRange = value;
+			}
+		}
+
 		float m_LastRelease = 0;
 		List<Sprite> m_BloodCellList = new List<Sprite>();
 		public List<Sprite> BloodCells
@@ -21,9 +59,9 @@ namespace project_hook
 			}
 		}
 
-		public BloodCellGenerator()
+		public BloodCellGenerator(int p_BloodCellMax)
 		{
-			for (int i = 0; i < 4; i++)
+			for (int i = 0; i < p_BloodCellMax; i++)
 			{
 				Collidable t_Blood = new Collidable("BloodCell", new Vector2(0, 0), 50, 50, TextureLibrary.getGameTexture("bloodcell", "1"),
 					0.75f, true, -MathHelper.PiOver2, Depth.BackGroundLayer.Upper, Collidable.Factions.Blood, 100, 25);
@@ -51,7 +89,7 @@ namespace project_hook
 						c.Enabled = true;
 						c.ToBeRemoved = false;
 
-						c.Center = new Vector2(Game.Random.Next(0, Game.graphics.GraphicsDevice.Viewport.Width), 0);
+						c.Center = new Vector2(Game.Random.Next(m_BloodCellMinSpawnRange, m_BloodCellMaxSpawnRange), 0);
 						c.Task = new TaskStraightVelocity(new Vector2(0, 100));
 						c.Faction = Collidable.Factions.Blood;
 						c.Rotation = -MathHelper.PiOver2;
