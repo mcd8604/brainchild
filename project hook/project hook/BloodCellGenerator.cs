bfe7279a269e0d71d65c2af8b8c28543b290db40
mxd1365 <mxd1365@ee130bea-b940-0410-ae26-83e4380c6a04>
2008-02-13 02:58:57 +0000

    BloodCellGenerator optimized

--- project hook/project hook/BloodCellGenerator.cs
+++ project hook/project hook/BloodCellGenerator.cs
@@ -13,11 +13,17 @@ namespace project_hook
 		float m_BloodCellDelay = 3.0f;
 		float m_LastRelease = 0;
 		Random m_RanX = new Random();
-		List<Sprite> m_SpriteList;
-
 		List<Collidable> m_BloodCellList = new List<Collidable>();
+		List<Sprite> m_BloodCellDrawList = new List<Sprite>();
+		public List<Sprite> BloodCells
+		{
+			get
+			{
+				return m_BloodCellDrawList;
+			}
+		}
 
-		public BloodCellGenerator(List<Sprite> p_SpriteList)
+		public BloodCellGenerator()
 		{
 			for(int i= 0; i < 4; i++)
 			{
@@ -27,43 +33,24 @@ namespace project_hook
 				t_Blood.BlendMode = SpriteBlendMode.Additive;
 				t_Blood.Enabled = false;
 				m_BloodCellList.Add(t_Blood);
-				p_SpriteList.Add(t_Blood);
-				m_SpriteList = p_SpriteList;
+				m_BloodCellDrawList.Add(t_Blood);
 			}
 		}
 
 		public void Update(GameTime p_Time)
 		{
+			foreach (Sprite s in m_BloodCellDrawList)
+				s.Update(p_Time);
+
 			m_LastRelease += (float)p_Time.ElapsedGameTime.TotalSeconds;
-
-			
-
-			//    if (c.ToBeRemoved)
-			//    {
-			//        m_BloodCellList.Remove(c);
-			//        Collidable t_Blood = new Collidable("BloodCell", new Vector2(0, 0), 50, 50, TextureLibrary.getGameTexture("bloodcell", "1"),
-			//            0.75f, true, -MathHelper.PiOver2, Depth.BackGroundLayer.Upper, Collidable.Factions.Blood, 100, 25);
-			//        t_Blood.setAnimation("bloodcell", 60);
-			//        t_Blood.BlendMode = SpriteBlendMode.Additive;
-			//        t_Blood.Enabled = false;
-			//        m_BloodCellList.Add(t_Blood);
-			//        m_SpriteList.Add(t_Blood);
-			//    }
-			//}
-
 			foreach (Collidable c in m_BloodCellList)
 			{
 				if (m_LastRelease >= m_BloodCellDelay)
 				{
 					if (c.ToBeRemoved || !c.Enabled)
 					{
-						if (c.ToBeRemoved)
-						{
-							if (!m_SpriteList.Contains(c))
-								m_SpriteList.Add(c);
-						}
-						c.ToBeRemoved = false;
 						c.Enabled = true;
+						c.ToBeRemoved = false;
 
 						c.Center = new Vector2(m_RanX.Next(0, Game.graphics.GraphicsDevice.Viewport.Width), 0);
 						c.Task = new TaskStraightVelocity(new Vector2(0, 100));
@@ -76,6 +63,14 @@ namespace project_hook
 						break;
 					}
 				}
+			}
+		}
+
+		public void Draw(SpriteBatch p_SpriteBatch)
+		{
+			foreach (Collidable c in m_BloodCellDrawList)
+			{
+				c.Draw(p_SpriteBatch);
 			}
 		}
 	}
