7f184e2bb3d05f814ecba7d11ad645e39c5b1f58
adn1646 <adn1646@ee130bea-b940-0410-ae26-83e4380c6a04>
2008-02-13 03:42:32 +0000

    Why have two lists in Blood Cell Gen?

--- project hook/project hook/BloodCellGenerator.cs
+++ project hook/project hook/BloodCellGenerator.cs
@@ -13,39 +13,39 @@ namespace project_hook
 		float m_BloodCellDelay = 3.0f;
 		float m_LastRelease = 0;
 		Random m_RanX = new Random();
-		List<Collidable> m_BloodCellList = new List<Collidable>();
-		List<Sprite> m_BloodCellDrawList = new List<Sprite>();
+		List<Sprite> m_BloodCellList = new List<Sprite>();
 		public List<Sprite> BloodCells
 		{
 			get
 			{
-				return m_BloodCellDrawList;
+				return m_BloodCellList;
 			}
 		}
 
 		public BloodCellGenerator()
 		{
-			for(int i= 0; i < 4; i++)
+			for (int i = 0; i < 4; i++)
 			{
-				Collidable t_Blood = new Collidable("BloodCell", new Vector2(0,0), 50, 50, TextureLibrary.getGameTexture("bloodcell", "1"),
+				Collidable t_Blood = new Collidable("BloodCell", new Vector2(0, 0), 50, 50, TextureLibrary.getGameTexture("bloodcell", "1"),
 					0.75f, true, -MathHelper.PiOver2, Depth.BackGroundLayer.Upper, Collidable.Factions.Blood, 100, 25);
 				t_Blood.setAnimation("bloodcell", 60);
 				t_Blood.BlendMode = SpriteBlendMode.Additive;
 				t_Blood.Enabled = false;
 				m_BloodCellList.Add(t_Blood);
-				m_BloodCellDrawList.Add(t_Blood);
 			}
 		}
 
 		public void Update(GameTime p_Time)
 		{
-			foreach (Sprite s in m_BloodCellDrawList)
+			foreach (Sprite s in m_BloodCellList)
+			{
 				s.Update(p_Time);
+			}
 
 			m_LastRelease += (float)p_Time.ElapsedGameTime.TotalSeconds;
-			foreach (Collidable c in m_BloodCellList)
+			if (m_LastRelease >= m_BloodCellDelay)
 			{
-				if (m_LastRelease >= m_BloodCellDelay)
+				foreach (Collidable c in m_BloodCellList)
 				{
 					if (c.ToBeRemoved || !c.Enabled)
 					{
@@ -58,9 +58,9 @@ namespace project_hook
 						c.Rotation = -MathHelper.PiOver2;
 						c.Health = c.MaxHealth;
 						c.Animation.StartAnimation();
-						
+
 						m_LastRelease = 0;
-						break;
+						return;
 					}
 				}
 			}
@@ -68,7 +68,7 @@ namespace project_hook
 
 		public void Draw(SpriteBatch p_SpriteBatch)
 		{
-			foreach (Collidable c in m_BloodCellDrawList)
+			foreach (Sprite c in m_BloodCellList)
 			{
 				c.Draw(p_SpriteBatch);
 			}
