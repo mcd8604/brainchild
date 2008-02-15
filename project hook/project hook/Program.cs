473c4e5609900d51c462a7bc76a42e72059a5501
adn1646 <adn1646@ee130bea-b940-0410-ae26-83e4380c6a04>
2008-02-15 01:55:12 +0000

    Slightly more informative for the end user :/

--- project hook/project hook/Program.cs
+++ project hook/project hook/Program.cs
@@ -14,16 +14,17 @@ namespace project_hook
 			try
 			{
 #endif
-			using (Game game = new Game())
-			{
-				game.Run();
-			}
+				using (Game game = new Game())
+				{
+					game.Run();
+				}
 #if CATCH
 			}
 			catch (System.Exception e)
 			{
 				Game.Out.WriteLine("Exception occured: " + e);
 				Game.Out.WriteLine("Process Terminated.");
+				System.Windows.Forms.MessageBox.Show("A Fatal Error has occurred.", "Fatal Error", System.Windows.Forms.MessageBoxButtons.OK, System.Windows.Forms.MessageBoxIcon.Error);
 			}
 #endif
 		}
