45454603838038d904e980ca4c275614caf7c3a1
adn1646 <adn1646@ee130bea-b940-0410-ae26-83e4380c6a04>
2008-03-14 16:33:24 +0000

    PhysicsDemo3 \o/ Still hacked together, but much better than Physics 2.

---
+++ project blob/demo/PhysicsDemo3/PhysicsDemo3/Point.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+
+namespace PhysicsDemo3
+{
+	public class Point
+	{
+		public Vector3 Position = Vector3.Zero;
+		public Vector3 Velocity = Vector3.Zero;
+		public Vector3 Acceleration = Vector3.Zero;
+		public Vector3 Force = Vector3.Zero;
+
+		public float mass = 1;
+
+		public Point(Vector3 startPosition)
+		{
+			Position = startPosition;
+		}
+
+		public Vector3 getCurrentPosition()
+		{
+			return Position;
+		}
+
+	}
+}
