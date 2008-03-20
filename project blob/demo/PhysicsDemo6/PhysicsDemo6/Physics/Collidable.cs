b3d6b5e9cb0db144f6530001dcee5633de0ade1a
adn1646 <adn1646@ee130bea-b940-0410-ae26-83e4380c6a04>
2008-03-20 21:20:22 +0000

    more comments

--- project blob/demo/PhysicsDemo6/PhysicsDemo6/Physics/Collidable.cs
+++ project blob/demo/PhysicsDemo6/PhysicsDemo6/Physics/Collidable.cs
@@ -26,16 +26,37 @@ namespace Physics
         /// <returns></returns>
         bool shouldPhysicsBlock(Point p);
 
-
-
+        /// <summary>
+        /// The Dot Product to the normal of this collidable surface, if applicable.
+        /// </summary>
+        /// <param name="pos"></param>
+        /// <returns></returns>
 		float DotNormal(Vector3 pos);
 
+        /// <summary>
+        /// The Normal to this collidable surface, if applicable.
+        /// </summary>
+        /// <returns></returns>
 		Vector3 Normal();
 
+        /// <summary>
+        /// The Plane along which this collidable lies, if applicable.
+        /// </summary>
+        /// <returns></returns>
 		Plane getPlane();
 
+        /// <summary>
+        /// This collidable was hit by a point, and this force was transferred into the collidable.
+        /// </summary>
+        /// <param name="at"></param>
+        /// <param name="f"></param>
 		void ApplyForce(Vector3 at, Vector3 f);
 
+        /// <summary>
+        /// This collidable was hit by a point, and this velocity was transferred into the collidable.
+        /// </summary>
+        /// <param name="at"></param>
+        /// <param name="v"></param>
 		void ImpartVelocity(Vector3 at, Vector3 v);
 	}
 }
