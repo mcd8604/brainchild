efcf8b3a417c49a08c4af4b64bcde2b645a039ea
mxd1365 <mxd1365@ee130bea-b940-0410-ae26-83e4380c6a04>
2008-03-24 23:08:21 +0000

    model adder updated

--- project blob/demo/WorldMakerDemo/WorldMakerDemo/ModelSelect.Designer.cs
+++ project blob/demo/WorldMakerDemo/WorldMakerDemo/ModelSelect.Designer.cs
@@ -31,6 +31,9 @@ namespace WorldMakerDemo
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.closeButton = new System.Windows.Forms.Button();
             this.LoadButton = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.ModelName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // listBox1
@@ -44,7 +47,7 @@ namespace WorldMakerDemo
             // 
             // closeButton
             // 
-            this.closeButton.Location = new System.Drawing.Point(167, 152);
+            this.closeButton.Location = new System.Drawing.Point(167, 344);
             this.closeButton.Name = "closeButton";
             this.closeButton.Size = new System.Drawing.Size(141, 41);
             this.closeButton.TabIndex = 1;
@@ -54,7 +57,7 @@ namespace WorldMakerDemo
             // 
             // LoadButton
             // 
-            this.LoadButton.Location = new System.Drawing.Point(12, 152);
+            this.LoadButton.Location = new System.Drawing.Point(12, 344);
             this.LoadButton.Name = "LoadButton";
             this.LoadButton.Size = new System.Drawing.Size(149, 41);
             this.LoadButton.TabIndex = 2;
@@ -62,17 +65,47 @@ namespace WorldMakerDemo
             this.LoadButton.UseVisualStyleBackColor = true;
             this.LoadButton.Click += new System.EventHandler(this.LoadButton_Click);
             // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(12, 152);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(296, 160);
+            this.listBox2.TabIndex = 3;
+            this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
+            // 
+            // ModelName
+            // 
+            this.ModelName.Location = new System.Drawing.Point(49, 318);
+            this.ModelName.Name = "ModelName";
+            this.ModelName.Size = new System.Drawing.Size(259, 20);
+            this.ModelName.TabIndex = 4;
+            this.ModelName.TextChanged += new System.EventHandler(this.ModelName_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 321);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Name:";
+            // 
             // ModelSelect
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(320, 224);
+            this.ClientSize = new System.Drawing.Size(320, 398);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ModelName);
+            this.Controls.Add(this.listBox2);
             this.Controls.Add(this.LoadButton);
             this.Controls.Add(this.closeButton);
             this.Controls.Add(this.listBox1);
             this.Name = "ModelSelect";
             this.Text = "ModelSelect";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -81,5 +114,8 @@ namespace WorldMakerDemo
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.Button closeButton;
         private System.Windows.Forms.Button LoadButton;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.TextBox ModelName;
+        private System.Windows.Forms.Label label1;
     }
 }
