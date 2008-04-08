0976bc677e45f636a302537c931b6fd84b74a6e4
jxw0533 <jxw0533@ee130bea-b940-0410-ae26-83e4380c6a04>
2008-04-08 20:55:02 +0000

    Project working with Camera and the CameraManager.

--- project blob/Project_blob/Engine/Camera.cs
+++ project blob/Project_blob/Engine/Camera.cs
@@ -29,32 +29,100 @@ namespace Engine
         /// <summary>
         /// Postition of the camera.
         /// </summary>
-        private Vector3 _position;
+        private Vector3 _position = Vector3.Zero;
         public Vector3 Position
         {
             get { return _position; }
-            set { _position = value; }
+            set
+            {
+                _position = value;
+                UpdateMatrices();
+            }
         }
 
         /// <summary>
-        /// Quaternion rotation of the camera.
+        /// Where the camera is looking at
         /// </summary>
-        //private Quaternion _rotation;
-        //public Quaternion Rotation
-        //{
-        //    get { return _rotation; }
-        //    set { _rotation = value; }
-        //}
+        private Vector3 _target = Vector3.UnitZ;
+        public Vector3 Target
+        {
+            get { return _target; }
+            set
+            {
+                _target = value;
+                UpdateMatrices();
+            }
+        }
 
         /// <summary>
-        /// World Matrix of the camera.
+        /// Camera's Up vector.
         /// </summary>
-        //private Matrix _world = Matrix.Identity;
-        //public Matrix World
-        //{
-        //    get { return _world; }
-        //    set { _world = value; }
-        //}
+        private Vector3 _up = Vector3.Up;
+        public Vector3 Up
+        {
+            get { return _up; }
+            set
+            {
+                _up = value;
+                UpdateMatrices();
+            }
+        }
+
+        /// <summary>
+        /// Perspective field of view.
+        /// </summary>
+        private float _fieldOfView = MathHelper.ToRadians(45.0f);
+        public float FieldOfView
+        {
+            get { return _fieldOfView; }
+            set
+            {
+                _fieldOfView = value;
+                UpdateMatrices();
+            }
+        }
+
+        /// <summary>
+        /// Aspect Ratio of the camera.
+        /// </summary>
+        private float _aspectRatio = 4f / 3f;
+        public float AspectRatio
+        {
+            get { return _aspectRatio; }
+            set
+            {
+                _aspectRatio = value;
+                UpdateMatrices();
+            }
+        }
+
+        /// <summary>
+        /// Distance to the near clipping plane.
+        /// </summary>
+        private float _nearPlane = 0.1f;
+        public float NearPlane
+        {
+            get { return _nearPlane; }
+            set
+            {
+                _nearPlane = value;
+                UpdateMatrices();
+            }
+        }
+
+        /// <summary>
+        /// Distance to the far clipping plane.
+        /// </summary>
+        private float _farPlane = 100.0f;
+        public float FarPlane
+        {
+            get { return _farPlane; }
+            set
+            {
+                _farPlane = value;
+                UpdateMatrices();
+            }
+        }
 
         /// <summary>
         /// Matrix containing coordinates of the camera.
@@ -77,55 +145,6 @@ namespace Engine
         }
 
         /// <summary>
-        /// Perspective field of view.
-        /// </summary>
-        private float _fieldOfView = MathHelper.ToRadians(45.0f);
-        public float FieldOfView
-        {
-            get { return _fieldOfView; }
-            set
-            {
-                _fieldOfView = value;
-            }
-        }
-
-        /// <summary>
-        /// Aspect Ratio of the camera.
-        /// </summary>
-        private float _aspectRatio = 0.0f;
-        public float AspectRatio
-        {
-            get { return _aspectRatio; }
-            set { _aspectRatio = value; }
-        }
-
-        /// <summary>
-        /// Distance to the near clipping plane.
-        /// </summary>
-        private float _nearPlane = 0.1f;
-        public float NearPlane
-        {
-            get { return _nearPlane; }
-            set
-            {
-                _nearPlane = value;
-            }
-        }
-
-        /// <summary>
-        /// Distance to the far clipping plane.
-        /// </summary>
-        private float _farPlane = 20.0f;
-        public float FarPlane
-        {
-            get { return _farPlane; }
-            set
-            {
-                _farPlane = value;
-            }
-        }
-
-        /// <summary>
         /// The trapezoid that contains everything that the camera can see.
         /// </summary>
         private BoundingFrustum _frustum;
@@ -133,76 +152,6 @@ namespace Engine
         {
             get { return _frustum; }
             set { _frustum = value; }
-        }
-
-        /// <summary>
-        /// The Vertex positions of the Frustum used to draw the frustum wireframe
-        /// </summary>
-        private VertexPositionColor[] _boundingFrustumDrawData;
-        public VertexPositionColor[] BoundingFrustumDrawData
-        {
-            get { return _boundingFrustumDrawData; }
-            set { _boundingFrustumDrawData = value; }
-        }
-
-        /// <summary>
-        /// The frustum indexes used to draw the frustum wireframe
-        /// </summary>
-        private int[] _boundingFrustumIndex;
-        public int[] BoundingFrustumIndex
-        {
-            get { return _boundingFrustumIndex; }
-            set { _boundingFrustumIndex = value; }
-        }
-
-        /// <summary>
-        /// Rotation speed of the camera.
-        /// </summary>
-        private float _rotationSpeed;
-        public float RotationSpeed
-        {
-            get { return _rotationSpeed; }
-            set { _rotationSpeed = value; }
-        }
-
-        /// <summary>
-        /// Movement speed of the camera.
-        /// </summary>
-        private float _translationSpeed;
-        public float TranslationSpeed
-        {
-            get { return _translationSpeed; }
-            set { _translationSpeed = value; }
-        }
-
-        /// <summary>
-        /// Camera's Up vector.
-        /// </summary>
-        private Vector3 _up = Vector3.Up;
-        public Vector3 Up
-        {
-            get { return _up; }
-            set { _up = value; }
-        }
-
-        /// <summary>
-        /// Camera's look at point in world space.
-        /// </summary>
-        private Vector3 _lookAt;
-        public Vector3 LookAt
-        {
-            get { return _lookAt; }
-            set { _lookAt = value; }
-        }
-
-        /// <summary>
-        /// The spot in 3d space where the camera is looking.
-        /// </summary>
-        private Vector3 _cameraReference = new Vector3(0, 0, 1);
-        public Vector3 CameraReference
-        {
-            get { return _cameraReference; }
-            set { _cameraReference = value; }
         }
 
         /// <summary>
@@ -216,41 +165,6 @@ namespace Engine
         public virtual void Update(GameTime gameTime)
         {
 
-        }
-
-        public virtual void SetPosition(Vector3 newPosition)
-        {
-
-        }
-
-        public virtual void Translate(Vector3 move)
-        {
-
-        }
-
-        public virtual void RotateX(float angle)
-        {
-
-        }
-
-        public virtual void RotateY(float angle)
-        {
-
-        }
-
-        public virtual void RotateZ(float angle)
-        {
-
-        }
-
-        public virtual void Reset()
-        {
-
-        }
-
-        public virtual void CreateBoundingFrustrumWireFrame()
-        {
-            
         }
 
         public virtual void UpdateMatrices()
