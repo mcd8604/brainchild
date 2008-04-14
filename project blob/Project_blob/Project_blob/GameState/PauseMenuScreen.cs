45d604cfad1dc4961128b258f86c82fa72872dc9
mcj2218 <mcj2218@ee130bea-b940-0410-ae26-83e4380c6a04>
2008-04-14 21:50:45 +0000

    Blah?

---
+++ project blob/Project_blob/Project_blob/GameState/PauseMenuScreen.cs
@@ -0,0 +1,54 @@
+using System;
+using Microsoft.Xna.Framework;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project_blob.GameState
+{
+    class PauseMenuScreen : MenuScreen
+    {
+        public PauseMenuScreen()
+            : base("Paused")
+        {
+            // Flag that there is no need for the game to transition
+            // off when the pause menu is on top of it.
+            IsPopup = true;
+
+            // Create our menu entries.
+            MenuEntry resumeGameMenuEntry = new MenuEntry("Resume Game");
+            MenuEntry quitGameMenuEntry = new MenuEntry("Quit Game");
+            
+            // Hook up menu event handlers.
+            resumeGameMenuEntry.Selected += OnCancel;
+            quitGameMenuEntry.Selected += QuitGameMenuEntrySelected;
+
+            // Add entries to the menu.
+            MenuEntries.Add(resumeGameMenuEntry);
+            MenuEntries.Add(quitGameMenuEntry);
+        }
+
+
+        void QuitGameMenuEntrySelected(object sender, EventArgs e)
+        {
+            const string message = "Are you sure you want to quit this game?";
+
+            MessageBoxScreen confirmQuitMessageBox = new MessageBoxScreen(message);
+
+            confirmQuitMessageBox.Accepted += ConfirmQuitMessageBoxAccepted;
+
+            ScreenManager.AddScreen(confirmQuitMessageBox);
+        }
+
+        void ConfirmQuitMessageBoxAccepted(object sender, EventArgs e)
+        {
+            LoadingScreen.Load(ScreenManager, false, new MainMenuScreen());
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            ScreenManager.FadeBackBufferToBlack(TransitionAlpha * 2 / 3);
+
+            base.Draw(gameTime);
+        }
+    }
+}
