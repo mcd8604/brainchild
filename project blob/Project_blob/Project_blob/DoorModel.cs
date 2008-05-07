2864b48b06ccdb6dd9c7eb1f915a49a0606cde03
adn1646 <adn1646@ee130bea-b940-0410-ae26-83e4380c6a04>
2008-05-07 19:47:00 +0000

    TaskEditor

--- project blob/Project_blob/Project_blob/DoorModel.cs
+++ project blob/Project_blob/Project_blob/DoorModel.cs
@@ -1,15 +1,24 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Physics2;
+using Microsoft.Xna.Framework;
 
 namespace Project_blob
 {
-    class DoorModel : StaticModel
+    class DoorModel : DynamicModel
     {
 
         public DoorModel() { }
 
-        public DoorModel(StaticModel p_Model) : base(p_Model) { }
+        public DoorModel(StaticModel p_Model) : base(p_Model) 
+        {
+            Tasks = new List<Physics2.Task>();
+            List<Vector3> patrolPoints = new List<Vector3>();
+            patrolPoints.Add(Vector3.Zero);
+            patrolPoints.Add(Vector3.One);
+            Tasks.Add(new TaskTranslate(patrolPoints, 1f));
+        }
 
         public DoorModel(String p_Name, String fileName, String audioName, TextureInfo p_TextureKey, List<short> rooms)
             :base(p_Name, fileName, audioName, p_TextureKey, rooms)
