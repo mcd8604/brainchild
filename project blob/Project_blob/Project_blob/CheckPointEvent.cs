ae6985de411127ad1e6d1bf98812e1c3bf606676
mcd8604 <mcd8604@users.noreply.github.com>
2008-05-11 21:25:20 +0000

    changed Area.CheckPoint to Area.StartPosition since that's all it's used for. CheckPoint event is working (try getting on the hubworld conveyor belt from the ground then jump off the world)

--- project blob/Project_blob/Project_blob/CheckPointEvent.cs
+++ project blob/Project_blob/Project_blob/CheckPointEvent.cs
@@ -10,11 +10,29 @@ namespace Project_blob
     [Serializable]
     public class CheckPointEvent : EventTrigger
     {
+        private Vector3 m_CheckPoint = Vector3.Zero;
+        public Vector3 CheckPoint
+        {
+            get
+            {
+                return m_CheckPoint;
+            }
+            set
+            {
+                m_CheckPoint = value;
+            }
+        }
+
         public CheckPointEvent() { }
+
+        public CheckPointEvent(Vector3 p_CheckPoint)
+        {
+            m_CheckPoint = p_CheckPoint;
+        }
 
         public void PerformEvent(PhysicsPoint point)
         {
-
+            GameplayScreen.SetCheckPoint(m_CheckPoint);
         }
     }
 }
