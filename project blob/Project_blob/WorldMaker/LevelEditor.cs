5fdbc799ad308dd76c1060ae6c776f2042d73029
mcd8604 <mcd8604@ee130bea-b940-0410-ae26-83e4380c6a04>
2008-05-15 02:40:13 +0000

    vertex buffer only updated on draw call

--- project blob/Project_blob/WorldMaker/LevelEditor.cs
+++ project blob/Project_blob/WorldMaker/LevelEditor.cs
@@ -323,7 +323,7 @@ namespace WorldMaker
 				{
 					_drawablesToDelete.Add(_gameRef.ActiveArea.Display.CurrentlySelected);
 					Console.WriteLine(_modelSelect.CurrentModel.Name);
-					_modelSelect.CurrentModel.updateTextureCoords();
+					//_modelSelect.CurrentModel.updateTextureCoords();
 					_drawableInfo.name = _modelSelect.CurrentModel.Name;
 					_drawableInfo.textureID = _modelSelect.CurrentModel.GetTextureID();
 					StaticModel temp = _modelSelect.CurrentModel;
