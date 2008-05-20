50d0e3c535aeb5b9e8b5b5e1430807e052dd1ffd
adn1646 <adn1646@ee130bea-b940-0410-ae26-83e4380c6a04>
2008-05-20 00:42:59 +0000

    split for debugging, so we can get an accurate line number

--- project blob/Project_blob/Project_blob/SpeedStatic.cs
+++ project blob/Project_blob/Project_blob/SpeedStatic.cs
@@ -19,7 +19,15 @@ namespace Project_blob
 
 		public override Vector3 getRelativeVelocity(CollisionEvent e)
 		{
-			return (Vector3.Cross(e.collidable.Normal, Vector3.Cross(e.point.ParentBody.getAverageVelocity(), e.collidable.Normal))) * bonus;
+			// debugging:
+
+			Vector3 av = e.point.ParentBody.getAverageVelocity();
+
+			Vector3 cross1 = Vector3.Cross(av , e.collidable.Normal);
+
+			Vector3 cross2 = Vector3.Cross(e.collidable.Normal, cross1);
+
+			return cross2 * bonus;
 		}
 	}
 }
