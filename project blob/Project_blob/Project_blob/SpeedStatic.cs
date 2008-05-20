3c0ecbc5b9c8d871900c86aebfc9357e2f066a64
adn1646 <adn1646@ee130bea-b940-0410-ae26-83e4380c6a04>
2008-05-20 01:11:50 +0000

    possible check / fix - unconfirmed

--- project blob/Project_blob/Project_blob/SpeedStatic.cs
+++ project blob/Project_blob/Project_blob/SpeedStatic.cs
@@ -20,14 +20,13 @@ namespace Project_blob
 		public override Vector3 getRelativeVelocity(CollisionEvent e)
 		{
 			// debugging:
-
-			Vector3 av = e.point.ParentBody.getAverageVelocity();
-
-			Vector3 cross1 = Vector3.Cross(av , e.collidable.Normal);
-
-			Vector3 cross2 = Vector3.Cross(e.collidable.Normal, cross1);
-
-			return cross2 * bonus;
+#if DEBUG
+			if (e.point.ParentBody == null)
+			{
+				throw new ArgumentNullException("A point collided with a speed tile that did not have a parent body!");
+			}
+#endif
+			return Vector3.Cross(e.collidable.Normal, Vector3.Cross(e.point.ParentBody.getAverageVelocity() , e.collidable.Normal)) * bonus;
 		}
 	}
 }
