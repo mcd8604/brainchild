28c85284a1dca0b4b7b62e8be3427a5c1e85e9d7
adn1646 <adn1646@ee130bea-b940-0410-ae26-83e4380c6a04>
2008-05-21 20:32:16 +0000

    large camera collision

--- project blob/Project_blob/Project_blob/CameraBody.cs
+++ project blob/Project_blob/Project_blob/CameraBody.cs
@@ -4,42 +4,84 @@ using System.Text;
 using Microsoft.Xna.Framework;
 using Physics2;
 
-namespace Project_blob {
+namespace Project_blob
+{
 
-	internal class CameraBody : Body {
+	internal class CameraBody : Body
+	{
 
 		PhysicsPoint point;
 		TaskCamera task;
 
-		public CameraBody(Body Target) {
+		public bool UseLargeCameraCollision = true;
+		public float CameraRadius = 2f;
+
+		public CameraBody(Body Target)
+		{
 			point = new PhysicsPoint(Vector3.Zero);
 			points.Add(point);
+
+			if (UseLargeCameraCollision)
+			{
+				for (int i = 0; i < 6; ++i)
+				{
+					points.Add(new PhysicsPoint(Vector3.Zero));
+				}
+			}
+
 			task = new TaskCamera(Target);
 			tasks.Add(task);
 
 			initialize();
 		}
 
-		public Vector3 getCameraPosition() {
-			return point.ExternalPosition;
+		public Vector3 getCameraPosition()
+		{
+			if (UseLargeCameraCollision)
+			{
+				Vector3 center = Vector3.Zero;
+				foreach (PhysicsPoint p in points)
+				{
+					center += p.ExternalPosition;
+					if (p.LastCollision != null)
+					{
+						center += p.LastCollision.Normal * CameraRadius;
+					}
+				}
+				return center /= points.Count;
+			}
+			else
+			{
+				return point.ExternalPosition;
+			}
 		}
 
-		public override void update(float TotalElapsedSeconds) {
+		public override void update(float TotalElapsedSeconds)
+		{
 			task.update(this, TotalElapsedSeconds);
 			boundingBox.clear();
-			boundingBox.expandToInclude(point.CurrentPosition);
-			boundingBox.expandToInclude(point.PotentialPosition);
+			foreach (Physics2.PhysicsPoint p in points)
+			{
+				boundingBox.expandToInclude(p.CurrentPosition);
+				boundingBox.expandToInclude(p.PotentialPosition);
+			}
 		}
 
-		public override void updatePosition() {
-			point.updatePosition();
+		public override void updatePosition()
+		{
+			foreach (Physics2.PhysicsPoint p in points)
+			{
+				p.updatePosition();
+			}
 		}
 
-		public void setCameraOffset(Vector3 Offset) {
+		public void setCameraOffset(Vector3 Offset)
+		{
 			task.OffsetVector = Offset;
 		}
 
-		public void setCameraTarget(Body Target) {
+		public void setCameraTarget(Body Target)
+		{
 			task.Target = Target;
 		}
 
